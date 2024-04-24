c979d7cf8bf534c7e7ebff8edab4fa89eb466c2c
Mad-ArrA <arkadi8377@gmail.com>
2024-04-24 15:29:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def something_wrong_here(a, b):
+    if a == b:
+        print('Тест пройден')
+    else:
+        print('Тест провален')
+
+
+something_wrong_here('C', 'C')
